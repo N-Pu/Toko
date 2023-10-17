aa3653e4b5aed0cf99254fcabe3b3ae7fbba6df2
N-Pu <garry.garyra@mail.ru>
2023-10-17 21:45:56 +0300

    feature/TOKO-13 #comment

--- app/src/main/java/com/project/toko/detailScreen/model/detailModel/old/AnimeDetailModel.kt
+++
@@ -1,9 +0,0 @@
-package com.project.toko.detailScreen.model.detailModel.old
-
-
-import com.google.gson.annotations.SerializedName
-import com.project.toko.homeScreen.model.newAnimeSearchModel.Data
-
-data class AnimeDetailModel(
-     @SerializedName("data") val data: Data
-)
