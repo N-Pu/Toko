35fdeafbee56acbd36ff65e17838cd854df59a60
Harry.G <40484678+N-Pu@users.noreply.github.com>
2023-12-21 14:42:22 +0300

    fix/TOKO-53 #comment major optimization in Detail, Home, Person, Character and Dao screens by adding lambda functions in parameters.

--- app/src/main/java/com/project/toko/characterDetailedScreen/presentation_layer/characterFull/CharacterFullScreen.kt
+++ app/src/main/java/com/project/toko/characterDetailedScreen/presentation_layer/characterFull/CharacterFullScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
