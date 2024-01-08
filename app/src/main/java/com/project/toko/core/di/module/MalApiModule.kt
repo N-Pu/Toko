ca9a5369057164b509f29bab25e1e1e721222400
Harry.G <40484678+N-Pu@users.noreply.github.com>
2024-01-08 13:46:28 +0300

    fix/TOKO-56 #comment fixed search and caching.
    1. Fixed lagging in the Main screen, there were too much recompositions during the process of searching.
    2. Fixed caching in main screen and detail screen.
    3. Fixed ui text spacing in main screen, detail screen, character and staff screen.
    4. Added new colors in refresh icon depending on what mode (light or dark) user is in.

--- app/src/main/java/com/project/toko/core/di/module/MalApiModule.kt
+++ app/src/main/java/com/project/toko/core/di/module/MalApiModule.kt
@@ -9,7 +9,6 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.Cache
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
